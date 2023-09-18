afd06aea7bd5a3609123b253da362234dc350361
Shravan <newshravank25@gmail.com>
2023-09-18 22:46:34 +0530

    improvements in code: added secure card

--- classes/card.py
+++ classes/card.py
@@ -16,3 +16,22 @@ class CreditCard:
             return True
         else:
             return False
+
+
+class SecureCreditCard(CreditCard):
+    def __init__(self, name, card, expiry, cvc, number, password):
+        CreditCard.__init__(self, name, card, expiry, cvc)
+        self.number = number
+        self.password = password
+        self.df1 = pd.read_csv("files/card_security.csv", dtype=str)
+
+    def validate(self):
+        customer_tuple = (self.card, self.expiry, self.cvc, self.name)
+        list_of_cards = list(self.df.itertuples(index=False, name=None))
+        if customer_tuple in list_of_cards:
+            card_tuple = (self.number, self.password)
+            list_of_secure_card = list(self.df1.itertuples(index=False, name=None))
+            if card_tuple in list_of_secure_card:
+                return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from classes.hotel import Hotel
 from classes.ticket import Ticket
-from classes.card import CreditCard
+from classes.card import SecureCreditCard
 
 
 hotel = Hotel("files/hotels.csv")
@@ -11,7 +11,9 @@ while 1:
         card = input("\nEnter the credit card number:")
         expiry = input("\nEnter the credit card expiry in MM/YY:")
         cvc = input("\nEnter the credit card CVC: ")
-        card_object = CreditCard(name, card, expiry, cvc)
+        user = input("\nEnter the username: ")
+        password = input("\nEnter the password: ")
+        card_object = SecureCreditCard(name, card, expiry, cvc, user, password)
         if card_object.validate():
             ticket = Ticket()
             print(ticket.print(name, hotel.hotel_name, hotel.hotel_city))
