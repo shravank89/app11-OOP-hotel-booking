6dfb7e034ec8a956acae64b02c461d81cc0df592
Shravan <newshravank25@gmail.com>
2023-09-18 22:24:51 +0530

    improvements in code

--- classes/hotel.py
+++ classes/hotel.py
@@ -4,14 +4,14 @@ import pandas as pd
 class Hotel:
     def __init__(self, path):
         self.df = pd.read_csv(path, dtype={"id": str})
-        print(self.df.to_string(index=False))
+        self.df_string = self.df.to_string(index=False)
+        print(self.df_string)
         self.hotel_id_choice = input("\nEnter the ID of Hotel you want to book: ")
-        self.hotel_name = self.df.loc[self.df["id"] == self.hotel_id_choice]["name"].squeeze()
-        self.hotel_city = self.df.loc[self.df["id"] == self.hotel_id_choice]["city"].squeeze()
-
 
     def check_availability(self):
-        while 1:
+        while True:
+            self.hotel_name = self.df.loc[self.df["id"] == self.hotel_id_choice]["name"].squeeze()
+            self.hotel_city = self.df.loc[self.df["id"] == self.hotel_id_choice]["city"].squeeze()
             if self.hotel_id_choice in list(self.df["id"]):
                 flag = self.df.loc[self.df["id"] == self.hotel_id_choice]["available"].squeeze()
                 if flag == "yes":
@@ -19,6 +19,7 @@ class Hotel:
                 else:
                     return False
             else:
-                print("Hotel not in list, please renter")
-                print(self.df)
-                self.hotel_id_choice = input("\nEnter the ID of Hotel you want to book: ")
+                print("\nHotel not in list, please renter")
+                print(self.df_string)
+                self.hotel_id_choice = input("\nEnter the ID of Hotel you want to book again: ")
+
--- main.py
+++ main.py
@@ -4,18 +4,20 @@ from classes.card import CreditCard
 
 
 hotel = Hotel("files/hotels.csv")
-
-if hotel.check_availability():
-    name = input("Enter the name of customer:")
-    name = name.strip().title()
-    card = input("Enter the credit card number:")
-    expiry = input("Enter the credit card expiry in MM/YY:")
-    cvc = input("Enter the credit card CVC: ")
-    card_object = CreditCard(name, card, expiry, cvc)
-    if card_object.validate():
-        ticket = Ticket()
-        print(ticket.print(name, hotel.hotel_name, hotel.hotel_city))
-else:
-    print("\nThis hotel is not free, try another")
-    hotel = Hotel("files/hotels.csv")
-
+while 1:
+    if hotel.check_availability():
+        name = input("\nEnter the name of customer:")
+        name = name.strip().title()
+        card = input("\nEnter the credit card number:")
+        expiry = input("\nEnter the credit card expiry in MM/YY:")
+        cvc = input("\nEnter the credit card CVC: ")
+        card_object = CreditCard(name, card, expiry, cvc)
+        if card_object.validate():
+            ticket = Ticket()
+            print(ticket.print(name, hotel.hotel_name, hotel.hotel_city))
+            break
+        else:
+            print("\nCard can not be validated, Transaction failed!")
+            break
+    else:
+        hotel.hotel_id_choice = input("\nThis hotel is not free, try another: ")
