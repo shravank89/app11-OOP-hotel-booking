ca0598fdf41d4fdf6497f71feca7000fa34336e9
Shravan <newshravank25@gmail.com>
2023-09-19 10:56:30 +0530

    improvements in code: customer can choose for 2Factor auth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from classes.hotel import Hotel
 from classes.ticket import Ticket
-from classes.card import SecureCreditCard
+from classes.card import SecureCreditCard, CreditCard
 
 
 hotel = Hotel("files/hotels.csv")
@@ -11,9 +11,13 @@ while 1:
         card = input("\nEnter the credit card number:")
         expiry = input("\nEnter the credit card expiry in MM/YY:")
         cvc = input("\nEnter the credit card CVC: ")
-        user = input("\nEnter the username: ")
-        password = input("\nEnter the password: ")
-        card_object = SecureCreditCard(name, card, expiry, cvc, user, password)
+        secure_prompt = input("\nDo you want to validate card with 2 factor authentication? Reply Y/N")
+        if secure_prompt == "Y" or "y":
+            user = input("\nEnter the username: ")
+            password = input("\nEnter the password: ")
+            card_object = SecureCreditCard(name, card, expiry, cvc, user, password)
+        else:
+            card_object = CreditCard(name, card, expiry, cvc)
         if card_object.validate():
             ticket = Ticket()
             print(ticket.print(name, hotel.hotel_name, hotel.hotel_city))
